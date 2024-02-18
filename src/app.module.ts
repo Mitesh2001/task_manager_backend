162665a025179de70122205e9b3470824d260427
Mitesh2001 <miteshladva.dgrs@gmail.com>
2024-02-19 00:31:13 +0530

    - installed nestjs/mongoose & mongoose - connect with local mongo db server

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot("mongodb://localhost:27017/task_manager"),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
