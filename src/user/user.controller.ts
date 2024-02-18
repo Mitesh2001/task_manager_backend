c66c20949835e688bf933c9604facbbac76d48f0
Ladva Mitesh <60682333+Mitesh2001@users.noreply.github.com>
2024-02-19 01:19:25 +0530

    user module created (#2)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { createUserDto } from './user.dto';
+import { User } from './user.entity';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
-    @Post('')
-    create() {
-        return this.userService.createUser();
+    @Post()
+    async createUser(@Body() createUserDto: createUserDto): Promise<User> {
+        return this.userService.createUser(createUserDto);
+    }
+
+    @Get()
+    async getAllUsers(): Promise<User[]> {
+        return this.userService.getAllUsers();
     }
 
 }
