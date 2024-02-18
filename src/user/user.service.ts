ee6876cfe78908be9b562695db82bd270e321f70
Mitesh2001 <miteshladva.dgrs@gmail.com>
2024-02-19 00:32:30 +0530

    user module created

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './user.entity';
+import { Model } from 'mongoose';
+import { createUserDto } from './user.dto';
 
 @Injectable()
 export class UserService {
-    constructor() { }
 
-    createUser = async () => {
-        return {
-            message: "New User Created successfully !"
-        }
+    constructor(@InjectModel(User.name) private readonly userModel: Model<User>) { }
+
+    createUser = async (createUserDto: createUserDto): Promise<User> => {
+        const createUser = new this.userModel(createUserDto);
+        return createUser.save();
+    }
+
+    getAllUsers = async (): Promise<User[]> => {
+        return this.userModel.find();
     }
 
 }
