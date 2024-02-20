d229b3528c538d7309e1c6441594c866b33f49e6
Ladva Mitesh <60682333+Mitesh2001@users.noreply.github.com>
2024-02-20 22:49:02 +0530

    Validation/task module (#3)
    * - installed nest js validation packages
    - setup validation pipe globally
    
    * validation applied
    
    * task update API created
    
    * common response for all the CRUD operation APIs

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Task } from './task.schema';
 import { Model } from 'mongoose';
-import { TaskCreateDto } from './task.dto';
+import { TaskCreateDto, TaskUpdateDto } from './task.dto';
 
 @Injectable()
 export class TaskService {
@@ -16,6 +16,26 @@ export class TaskService {
 
     getAllTask = async (): Promise<Task[]> => {
         return this.taskModel.find();
+    }
+
+    getById = async (taskId: string): Promise<Task> => {
+        const task = this.taskModel.findById(taskId);
+        if (!task) {
+            throw new NotFoundException(`Task with ID ${taskId} not found`);
+        }
+        return task;
+    }
+
+    updateById = async (taskId: string, taskDetails: TaskUpdateDto): Promise<Task> => {
+        const updatedTask = await this.taskModel.findByIdAndUpdate(taskId, { ...taskDetails, lastUpdatedDate: new Date().toISOString() }, { new: true });
+        if (!updatedTask) {
+            throw new NotFoundException(`Task with ID ${taskId} not found`);
+        }
+        return updatedTask;
+    }
+
+    deleteById = async (taskId: string): Promise<void> => {
+        this.taskModel.findByIdAndDelete(taskId).exec();
     }
 
 }
